234da1db6570815d4a681f58b8482890b787aaa5
chmufmemmrr0 <d.rafal7939@gmail.com>
2026-02-20 13:13:22 +0100

    added homepage boom animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Gallery from './pages/Gallery'
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
+import './../index.css'
+import { motion } from 'motion/react'
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex items-center justify-center h-screen bg-yellow-100/50">
+            <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: [0, 1.1, 1] }}
+                transition={{ duration: 0.75, ease: 'easeInOut' }}
+                className="w-1/2 h-128 relative"
+            >
+                <motion.div 
+                    animate={{
+                        x: [1, -1, -3, 3, 1, -1, -3, 3, -1, 1, 1],
+                        y: [1, -2, 0, 2, -1, 2, 1, 1, -1, 2, -2],
+                        rotate: [0, -1, 1, 0, 1, -1, 0, -1, 1, 0, -1]
+                    }}
+                    transition={{ duration: 0.5, ease: 'linear', repeat: Infinity }}
+                    className="absolute bg-[url('src/assets/HomeBoomBackground.png')] bg-cover bg-center w-full h-full"
+                ></motion.div>
+                <div className="scale-90 absolute bg-[url('src/assets/HomeBoomForeground.png')] z-10 bg-cover bg-center w-full h-full"></div>
+            </motion.div>
         </div>
     )
 }
